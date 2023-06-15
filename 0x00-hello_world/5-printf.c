9b355c8a215483d99cb94baf1fda0615e9c09786
ZackAtambo <zacharyatambo@gmail.com>
2023-06-15 10:15:46 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main -program that prints a line
-* Return: 0
-*/
+ * main - Entry point
+ * Return: 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
