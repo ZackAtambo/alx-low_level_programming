76ac271ce4da80240cf901b963a33d06d9124938
ZackAtambo <zacharyatambo@gmail.com>
2023-06-20 14:02:20 -0700

    102-print_comb5.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
-
 #include "main.h"
 
 
 	/**
 	 * print_sign - prints the sign of a number
 	 * @n: the int to check
+	 * Description: This function takes an integer @n as an argument
+	 * and prints the sign of the number
 	 * Return: 1 and prints + if n is greater than zero
 	 * 0 and prints 0 if n is zero
 	 * -1 and prints - if n is less than zero
